db1215673706f03c7a15f3a931575cc8de542850
Mike Bayer <mike_mp@zzzcomputing.com>
2014-12-30 10:32:53 -0500

    - give up on trying to counteract left-scroll effects.  not worth it as it looks crappy esp on ios.

--- zzzeeksphinx/themes/zzzeeksphinx/static/init.js
+++ zzzeeksphinx/themes/zzzeeksphinx/static/init.js
@@ -13,34 +13,14 @@ function initFloatyThings() {
     // of whether or not we are moving with the page or not because
     // we want it to have an independently-moving scrollbar at all
     // times.
-    // this unfortunately means we have to keep it steady across
-    // page scrolls, vertically and horizontally, in negative scrolls
-    // as well for safari + chrome and also handle resizes.  these browsers
-    // all do something a little different with positioning and jquery
-    // does not seem to abstract against this use case well.
+    // this unfortunately means we either have to keep it steady across
+    // page scrolls or deal with the fact that the text is flowing
+    // under it in some resize/side-scroll scenarios.
 
     var automatedBreakpoint = $("#docs-container").position().top +
         $("#docs-top-navigation-container").height();
 
-    // "top" seems to stay constant...
     var docsBodyOffset = $("#docs-body").offset().top;
-
-    var top = docsBodyOffset;
-    var left;
-
-    function resize() {
-        // ...while the "left" seems to change based on doc
-        // resizes, e.g. if you make the safari window lots bigger
-        // than the page
-        left = $("#docs-body-container").offset();
-        if (left) {
-            left = left.left;
-        } // otherwise might be undefined
-        else {
-            left = 0;
-        }
-        setScroll();
-    }
 
     // this turns on the whole thing, without this
     // we are in graceful degradation assuming no JS
@@ -48,11 +28,11 @@ function initFloatyThings() {
 
     function setScroll() {
         var scrolltop = $(window).scrollTop();
-        var scrollleft = $(window).scrollLeft();
+        var fix = scrolltop >= automatedBreakpoint;
 
         // when page is scrolled down past the top headers,
         // sidebar stays fixed vertically
-        if (scrolltop >= automatedBreakpoint) {
+        if (fix) {
             $("#fixed-sidebar.withsidebar").css("top", 5);
         }
         else if (scrolltop < 0) {
@@ -69,20 +49,17 @@ function initFloatyThings() {
                 "top", docsBodyOffset - scrolltop);
         }
 
-        // adjust left scroll.
-        // chrome has a "springy" zone in its scrollbar that does
-        // not actually move the elements, and this basically means we
-        // are breaking the spacing in those zones,  so the navbar "springs"
-        // around with this action.   jquery needs to provide a
-        // "what is the actual position of the *document* in response to
-        // scrolling" feature, not just the raw value of a scrollbar.
-        $("#fixed-sidebar.withsidebar").css(
-            "left", left - scrollleft);
+        // adjusting left scroll is also an option,
+        // but doesn't seem to be worth it, safari is the only browser
+        // that shows much of a change, and overall the adjustment here
+        // is jerky and error-prone esp. on lesser browsers like safari ipad.
+        // looking at our "mentor" documentation, they don't do this;
+        // they just have the whole layout such that you don't really notice
+        // the horizontal squeezing as much (nav is on the right, they don't
+        // have a border around the text making it obvious).
     }
     $(window).scroll(setScroll);
-    $(window).resize(resize);
-
-    resize();
+    setScroll();
 }
 
 function highlightLinks() {
