6dccda701f1f5413f9a33dfdd8f20b4e01c77dca
Mike Bayer <mike_mp@zzzcomputing.com>
2015-03-24 11:40:09 -0400

    - add some more default attributes to help w/ canonical urls

--- zzzeeksphinx/mako.py
+++ zzzeeksphinx/mako.py
@@ -70,6 +70,9 @@ class MakoBridge(TOCMixin, TemplateBridge):
         context.setdefault('toc', None)
         context.setdefault('pdf_url', None)
         context.setdefault('metatags', None)
+        context.setdefault("canonical_url", None)
+        context.setdefault("single_version", None)
+        context.setdefault("rtd_language", "en")
         # override context attributes
         self.setup_ctx(context)
         context.setdefault('_', lambda x: x)
