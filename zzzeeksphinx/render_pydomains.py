693f3e2a8e5b7b65c9bab539911182b96a7563b5
Mike Bayer <mike_mp@zzzcomputing.com>
2020-04-10 23:24:18 -0400

    Implement for classes, functions, fixes

--- zzzeeksphinx/render_pydomains.py
+++ zzzeeksphinx/render_pydomains.py
@@ -1,52 +1,61 @@
-import re
 from docutils import nodes
 from docutils.transforms import Transform
-import os
-from sphinx.util.osutil import copyfile
-from sphinx.util.console import bold
-from sphinx.domains.python import PyXRefRole
-from sphinx.domains.python import PythonDomain
-from distutils.version import LooseVersion
-from sphinx import __version__
 from sphinx.addnodes import pending_xref
+import re
 
 # the searchindex.js system relies upon the object types
 # in the PythonDomain to create search entries
-from sphinx.domains import ObjType
-from sphinx.util import logging
 
 
 class RenderDocLinks(Transform):
-    # apply references targets and optional references
-    # to nodes that contain our target text.
     default_priority = 210
 
     def apply(self):
-        classes = {"py-func", "py-attr", "py-meth", "py-class"}
-
-        reg = re.compile(r"^(:meth:|:attr:)`~?(.+)`$")
-
-        def find_code_node(node):
-            return isinstance(node, nodes.literal) and classes.intersection(
-                node.attributes.get("classes", ())
-            )
-
         py_nodes = self.document.traverse(pending_xref)
         for py_node in py_nodes:
-            if py_node.attributes['reftype'] in ('meth', 'attr', 'paramref'):
-                if py_node.attributes['reftype'] == 'paramref':
+            if not py_node.children or not py_node.children[0].children:
+                continue
+
+            reftype = py_node.attributes["reftype"]
+            if reftype in ("meth", "attr", "paramref"):
+                tokens = py_node.attributes["reftarget"].split(".")
+                lt = len(tokens)
+                if (
+                    reftype == "paramref"
+                    and lt >= 3
+                    and tokens[-3][0].isupper()
+                ):
+                    # for paramref look at first char of "method" token
+                    # to see if its a method name or if this is a
+                    # function.  paramrefs don't store this info right now.
                     need = 3
                 else:
-                    need = 2
-                tokens = py_node.attributes['reftarget'].split(".")
+                    need = min(lt, 2)
                 corrected_name = ".".join(tokens[-need:])
-                if py_node.attributes["reftype"] == "meth":
-                    corrected_name += "()"
-                if str(py_node.children[0].children[0]) != corrected_name:
-                    py_node.children[0].pop(0)
-                    py_node.children[0].insert(
-                        0, nodes.Text(corrected_name, corrected_name)
-                    )
+            elif reftype == "func":
+                tokens = py_node.attributes["reftarget"].split(".")
+                corrected_name = tokens[-1]
+                need = 1
+            elif reftype == "class" and re.match(
+                r"^:class:`\..+`$", py_node.rawsource
+            ):
+                # for a class node, only rewrite it if the rawsource
+                # indicates a "." at the beginning.  otherwise it either
+                # has a ~ and it will just be class name anyway, or
+                # it's fully module qualified and that should write out fully.
+                tokens = py_node.attributes["reftarget"].split(".")
+                corrected_name = tokens[-1]
+                need = 1
+            else:
+                continue
+
+            if py_node.attributes["reftype"] in ("meth", "func"):
+                corrected_name += "()"
+
+            py_node.children[0].pop(0)
+            py_node.children[0].insert(
+                0, nodes.Text(corrected_name, corrected_name)
+            )
 
 
 def setup(app):
