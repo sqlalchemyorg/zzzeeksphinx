18fbc15cb557a3f5176107564abb57173202e815
Mike Bayer <mike_mp@zzzcomputing.com>
2020-04-11 18:38:10 -0400

    - dev

--- tools/fix_xrefs.py
+++ tools/fix_xrefs.py
@@ -11,9 +11,6 @@ NORMAL = "\033[0m"
 UNDERLINE = "\033[4m"
 
 
-reg = re.compile(r"\:(class|attr|func|meth|paramref)\:`(.+?)`")
-
-
 def _token_to_str(token):
     if isinstance(token, str):
         return token
@@ -32,6 +29,14 @@ def highlighted(line_tokens, match_idx, group):
 
 
 def prompt(fname, lines, linenum, line_tokens, token_idx, rec, replacements):
+    """Present the prompt screen for a single token in a line in a file and
+    receive user input.
+
+    handle_line() calls this function repeated times for a given token
+    in a loop until the user command indicates we are done working with
+    this particular token.
+
+    """
     context_lines = 12
     print("\033c")
     print("-----------------------------------------------------------------")
@@ -93,18 +98,72 @@ def prompt(fname, lines, linenum, line_tokens, token_idx, rec, replacements):
             return replacement_index - 1
 
 
-def tokenize_line(line):
+reg = re.compile(r"\:(class|attr|func|meth|paramref)\:`(.+?)(?:\(\))?`")
+
+
+def tokenize_line(line, max_missing_package_tokens=0):
+    """Search a line for py references.
+
+    Return the line as as list of tokens, with non matched portions
+    and matched portions together, using Match objects for the matches
+    and plain strings for the non matched.
+
+    max_missing_package_tokens indicates which xrefs we will prompt
+    for, based on how many package tokens are not present.  zero means
+    only xrefs that are straight class, class + method, function etc.
+    with no package qualification at all.
+
+    """
+
     tokens = []
     start = -1
     mend = 0
     has_tokens = False
     for match in reg.finditer(line):
-        has_tokens = True
+
+        py_type = match.group(1)
+        symbol = match.group(2).lstrip('~.')
+        py_tokens = symbol.split(".")
+        if py_type == "class":
+            target = py_tokens.pop(-1)  # noqa
+            package_tokens = py_tokens
+        elif py_type in ("meth", "attr"):
+            methname = py_tokens.pop(-1)
+            if py_tokens:
+                clsname = py_tokens.pop(-1)
+                target = "%s.%s" % (clsname, methname)
+            package_tokens = py_tokens
+        elif py_type == "func":
+            target = py_tokens.pop(-1)  # noqa
+            package_tokens = py_tokens
+        elif py_type == "paramref":
+            paramname = py_tokens.pop(-1)  # noqa
+            meth_or_fn_name = py_tokens.pop(-1)  # noqa
+            if py_tokens and py_tokens[-1][0].isupper():
+                clsname = py_tokens.pop(-1)
+            else:
+                clsname = None
+            package_tokens = py_tokens
+
+        if len(package_tokens) > max_missing_package_tokens:
+            is_match = False
+        else:
+            is_match = True
+
+        if is_match:
+            has_tokens = True
         mstart, mend = match.span(0)
         if start == -1:
             start = 0
         tokens.append(line[start:mstart])
-        tokens.append(match)
+
+        if is_match:
+            tokens.append(match)
+        else:
+            # append match as the string it matched so it won't be
+            # processed
+            tokens.append(match.group(0))
+
         start = mend
     tokens.append(line[mend:])
 
@@ -115,6 +174,14 @@ def tokenize_line(line):
 
 
 def process(fname, state):
+    """Parse, process, and write a single file.
+
+    Creates a list of lines and then passes each one off to handle_line().
+    handle_line() then has the option to replace that line in the list
+    of lines.   The list of lines is then rejoined to write out the new file.
+
+    """
+
     write = False
     with open(fname) as file_:
         lines = list(file_)
@@ -129,13 +196,13 @@ def process(fname, state):
         elif result == "c":  # has changes but keep going
             write = True
     if write:
-        write_lines(fname, lines)
+        with open(fname, "w") as file_:
+            file_.write("".join(lines))
 
-def write_lines(fname, lines):
-    with open(fname, "w") as file_:
-        file_.write("".join(lines))
 
 def handle_line(fname, state, lines, linenum, line):
+    """Parse, process and replace a single line in a list of lines."""
+
     line_tokens = tokenize_line(line)
 
     if not line_tokens:
@@ -197,13 +264,23 @@ def handle_line(fname, state, lines, linenum, line):
         lines[linenum] = newline
         return "c"
 
+
 def reformat_line(line_tokens, length=79):
+    """Given line tokens where one or more of the tokens has been replaced,
+    write out a new line, while ensuring that the max length is maintained.
+
+    When the resulting line would be longer than the length, the line is
+    split at that point.    Heuristics are used to determine what the
+    left-leading indentation should be, as well as if individual lines
+    have quotes on both sides.
+
+    """
     line_tokens = [_token_to_str(token) for token in line_tokens]
     printed_line = "".join(line_tokens)
     if len(printed_line) <= length:
         return printed_line
 
-    whitespace = re.match(r'^( +)', printed_line)
+    whitespace = re.match(r"^( +)", printed_line)
     if whitespace:
         whitespace = whitespace.group(1)
     else:
@@ -211,7 +288,7 @@ def reformat_line(line_tokens, length=79):
 
     quote_char = ""
     stripped = printed_line.strip()
-    if stripped[0] in '\'"':
+    if stripped[0] in "'\"":
         quote_char = stripped[0]
         if stripped[-1] != quote_char:
             quote_char = ""
@@ -233,7 +310,12 @@ def reformat_line(line_tokens, length=79):
 
 state_file_name = "fix_xref_state.txt"
 
+
 def restore_state_file():
+    """Read the state file if any and restore existing replacement tokens
+    that were established from a previous run.
+
+    """
     state = {}
     if not os.path.exists(state_file_name):
         return state
@@ -254,7 +336,9 @@ def restore_state_file():
 
     return state
 
+
 def write_replacement_rec(old, new):
+    """Write a single replacement token to the state file."""
     with open(state_file_name, "a") as file_:
         file_.write("%s %s\n" % (old, new))
 
