aaaa0e3aba69f0d6e5798eaed75ad3e031a1aed1
Mike Bayer <mike_mp@zzzcomputing.com>
2020-09-29 10:14:46 -0400

    css

--- zzzeeksphinx/extras.py
+++ zzzeeksphinx/extras.py
@@ -26,54 +26,16 @@ def depart_deepalchemy(self, node):
 deepalchemy_visit = (visit_deepalchemy, depart_deepalchemy)
 
 
-class observations(Admonition, Element):
-    pass
-
-
-class Observations(BaseAdmonition):
-    required_arguments = 0
-    node_class = observations
-
-
-def visit_observations(self, node):
-    self.visit_admonition(node, "observations")
-
-
-def depart_observations(self, node):
-    self.depart_admonition(node)
-
-
-observations_visit = (visit_observations, depart_observations)
-
-
 def setup(app):
 
     admonitionlabels["deepalchemy"] = _("Deep Alchemy")
-    admonitionlabels["observations"] = _("extra notes on the above example")
 
     app.add_directive("deepalchemy", DeepAlchemy)
-    app.add_directive("observations", Observations)
 
     app.add_node(
         deepalchemy,
         **{
             key: deepalchemy_visit
-            for key in [
-                "html",
-                "html5",
-                "latex",
-                "text",
-                "xml",
-                "texinfo",
-                "manpage",
-            ]
-        }
-    )
-
-    app.add_node(
-        observations,
-        **{
-            key: observations_visit
             for key in [
                 "html",
                 "html5",
