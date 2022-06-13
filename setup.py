7fb578f4eea33bf59fa21933fc18c40b04e98740
Mike Bayer <mike_mp@zzzcomputing.com>
2022-06-12 21:52:38 -0400

    pin upper at sphinx 5.1, ive been testing 5.0.1...

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     url="https://github.com/sqlalchemyorg/zzzeeksphinx",
     license="MIT",
     packages=["zzzeeksphinx"],
-    install_requires=["libsass", "mako", "requests", "sphinx>=4.5.0,<5"],
+    install_requires=["libsass", "mako", "requests", "sphinx>=4.5.0,<5.1"],
     include_package_data=True,
     zip_safe=False,
     entry_points={
